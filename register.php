e248374ba370697eb7013d529019f6b86bcfb903
Hemolqui <hernanhjmq123456@gmail.com>
2023-08-19 23:15:26 -0400

    v1

--- register.php
+++ register.php
@@ -1,6 +1,12 @@
 <?php
 
+// Configurar los encabezados CORS
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Headers: Content-Type");
+
 header("Content-Type: application/json; charset=UTF-8");
+
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $data = json_decode(file_get_contents("php://input"), true);
 
